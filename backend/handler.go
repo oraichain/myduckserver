bd9fe5b1c98f4093cca98ce09eef2bb0ac135161
TianyuZhang1214 <neo@apecloud.com>
2024-10-18 11:51:18 +0800

    to #98 fix: remove `AOTU_INCREMENT=\d`

--- backend/handler.go
+++ backend/handler.go
@@ -17,6 +17,8 @@ package backend
 import (
 	"context"
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/dolthub/go-mysql-server/server"
 	"github.com/dolthub/vitess/go/mysql"
@@ -26,6 +28,9 @@ type MyHandler struct {
 	*server.Handler
 	pool *ConnectionPool
 }
+
+// Precompile regex for performance
+var autoIncrementRegex = regexp.MustCompile(`AUTO_INCREMENT=\d+`)
 
 func (h *MyHandler) ConnectionClosed(c *mysql.Conn) {
 	h.pool.CloseConn(c.ConnectionID)
@@ -48,6 +53,11 @@ func (h *MyHandler) ComQuery(
 	query string,
 	callback mysql.ResultSpoolFn,
 ) error {
+	// https://github.com/dolthub/dolt/issues/8455
+	query = strings.ReplaceAll(query, "CHARACTER SET 'utf8mb4'", "CHARACTER SET utf8mb4")
+
+	query = autoIncrementRegex.ReplaceAllString(query, "")
+
 	return h.Handler.ComQuery(ctx, c, query, callback)
 }
 
