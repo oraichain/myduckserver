916bb5844e0ffbc04d2b10b6cc9583a002e11da8
Fan Yang <fanyang@apecloud.com>
2024-12-24 01:38:53 +0800

    fix: correct snapshot.sh

--- devtools/replica-setup-mysql/snapshot.sh
+++ devtools/replica-setup-mysql/snapshot.sh
@@ -84,6 +84,6 @@ fi
 echo "Snapshot completed successfully."
 
 echo "Reset replica_is_loading_snapshot..."
-mysqlsh --sql --host=${MYDUCK_HOST} --port=${MYDUCK_PORT}  --user=${MYDUCK_USER} {MYDUCK_PASSWORD_OPTION} <<EOF
+mysqlsh --sql --host=${MYDUCK_HOST} --port=${MYDUCK_PORT}  --user=${MYDUCK_USER} ${MYDUCK_PASSWORD_OPTION} <<EOF
 SET GLOBAL replica_is_loading_snapshot = OFF;
 EOF
