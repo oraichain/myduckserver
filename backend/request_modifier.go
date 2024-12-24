ebc6d05ec1718a7b7d6b9958f0be1f7027d35ba9
Fan Yang <fanyang@apecloud.com>
2024-12-24 20:18:53 +0800

    fix: replace mariadb's utf8mb4_uca1400_ai_ci with utf8mb4_0900_ai_ci (#312)

--- backend/request_modifier.go
+++ backend/request_modifier.go
@@ -1,10 +1,19 @@
 package backend
 
+import "strings"
+
 // RequestModifier is a function type that transforms a query string
 type RequestModifier func(string, *[]ResultModifier) string
 
 // default request modifier list
 var defaultRequestModifiers = []RequestModifier{}
+
+// Newer MariaDB versions use utf8mb4_uca1400_ai_ci as the default collation,
+// which is not supported by go-mysql-server.
+// This function replaces the collation with the MySQL default utf8mb4_0900_ai_ci.
+func replaceMariaDBCollation(query string, _ *[]ResultModifier) string {
+	return strings.ReplaceAll(query, " utf8mb4_uca1400_ai_ci", " utf8mb4_0900_ai_ci")
+}
 
 // applyRequestModifiers applies request modifiers to a query
 func applyRequestModifiers(query string, requestModifiers []RequestModifier) (string, []ResultModifier) {
