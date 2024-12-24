87bb935391e95bd6ee4269215931184bc54a26dd
Fan Yang <fanyang@apecloud.com>
2024-12-24 19:54:15 +0800

    refactor: remove outdated query modifiers (#311)

--- backend/request_modifier.go
+++ backend/request_modifier.go
@@ -1,34 +1,10 @@
 package backend
 
-import "regexp"
-
 // RequestModifier is a function type that transforms a query string
 type RequestModifier func(string, *[]ResultModifier) string
 
-// Precompile regex for performance
-var autoIncrementRegex = regexp.MustCompile(`(?i)AUTO_INCREMENT=\d+`)
-var showSlaveStatusRegex = regexp.MustCompile(`(?i)^show\s+slave\s+status\s*;?$`)
-
 // default request modifier list
-var defaultRequestModifiers = []RequestModifier{
-	replaceAutoIncrement,
-	replaceShowSlaveStatus,
-}
-
-func replaceAutoIncrement(query string, modifiers *[]ResultModifier) string {
-	if autoIncrementRegex.MatchString(query) {
-		query = autoIncrementRegex.ReplaceAllString(query, "")
-	}
-	return query
-}
-
-func replaceShowSlaveStatus(query string, modifiers *[]ResultModifier) string {
-	if showSlaveStatusRegex.MatchString(query) {
-		*modifiers = append(*modifiers, replaceShowSlaveStatusFieldNames)
-		query = showSlaveStatusRegex.ReplaceAllString(query, "SHOW REPLICA STATUS;")
-	}
-	return query
-}
+var defaultRequestModifiers = []RequestModifier{}
 
 // applyRequestModifiers applies request modifiers to a query
 func applyRequestModifiers(query string, requestModifiers []RequestModifier) (string, []ResultModifier) {
